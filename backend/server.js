034956bba96298d108c053d9792ee04a2f954a1f
pijohasedev <pijohase.dev@gmail.com>
2025-12-27 12:05:49 +0800

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.use('/api/automation', automationRoutes);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
